5a9b319c887d3d8509181dabee43de54cf59bcf5
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2021-06-06 23:49:24 +0300

    added first console programm

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+      /// <summary>
+      /// Ветвления
+      /// </summary>
+      /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string str = "Hello Vasyl";
+            foreach (var item in str)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
