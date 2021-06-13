7c62df56531fceb51d111de9ae67234d9afc10b0
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2021-06-13 10:45:39 +0300

    Update Program.cs
    create array of strings, calculator

--- Program.cs
+++ Program.cs
@@ -4,18 +4,117 @@ namespace ConsoleApp1
 {
     class Program
     {
-      /// <summary>
-      /// Ветвления
-      /// </summary>
-      /// <param name="args"></param>
+
         static void Main(string[] args)
         {
-            string str = "Hello Vasyl";
-            foreach (var item in str)
+            #region array names
+            // Создание массива строк
+            /*int size;
+            Console.WriteLine("Введите размер создаваемого массива");
+            int.TryParse(Console.ReadLine(), out size);
+            string[] names = new string[size];
+            for(int i =0;i<names.Length;i++)
             {
-                Console.WriteLine(item);
+                
+                Console.WriteLine($"Введите имя для {i+1} элемента");
+                names[i] = Console.ReadLine();
+               
             }
+
+            Console.WriteLine($"Вы создали массив строк \"names\" на {size} элементов: \n");
+            foreach (var name in names)
+            {
+                Console.Write("\t" + name + "\n");
+            }*/
+            #endregion
+
+            #region calculator
+            double firstNumber;
+            double secondNumber;
+            double result;
+            char action;
+            Console.WriteLine("Enter the first number");
+            double.TryParse(Console.ReadLine(), out firstNumber);
+            Console.WriteLine("Enter the second number");
+            double.TryParse(Console.ReadLine(), out secondNumber);
+            Console.WriteLine("select the action to be performed: '+', '-', '/', '*', '%'");   // '+', '-', '/', '*', '%'
+            char.TryParse(Console.ReadLine(), out action);
+
+            //if (action != '+' || action != '-'|| action != '+'||action != '+'||action != '+')
+
+            switch (action)
+            {
+                case '+':
+                    result = firstNumber + secondNumber;
+                    Console.WriteLine("{0}  {1}  {2} = {3}", firstNumber, action, secondNumber, result);
+                    break;
+                case '-':
+                    result = firstNumber - secondNumber;
+                    Console.WriteLine("{0}  {1}  {2} = {3}", firstNumber, action, secondNumber, result);
+                    result = firstNumber - secondNumber;
+                    break;
+                case '*':
+                    result = firstNumber * secondNumber;
+                    Console.WriteLine("{0}  {1}  {2} = {3}", firstNumber, action, secondNumber, result);
+                    result = firstNumber * secondNumber;
+                    break;
+                case '/':
+
+                    if (secondNumber != 0)
+                    {
+                        result = firstNumber / secondNumber;
+                        Console.WriteLine("{0}  {1}  {2} = {3}", firstNumber, action, secondNumber, result);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Division by zero, action cannot be performed");
+                        break;
+                    }
+                case '%':
+                    if (secondNumber != 0)
+                    {
+                        result = firstNumber % secondNumber;
+                        Console.WriteLine("{0}  {1}  {2} = {3}", firstNumber, action, secondNumber, result);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Division by zero, action cannot be performed");
+                        break;
+                    }
+
+                default:
+
+                    Console.WriteLine("Enter valid data and correct actions!");
+                    break;
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            #endregion
+
+            Console.ReadKey();
+
+
+
+
         }
+
 
     }
 }
