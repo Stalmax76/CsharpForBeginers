4e53c9db5c117963c2ff81eac2e510fe829845fa
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2021-07-11 13:41:12 +0300

    Update Program.cs
    Added - Regiones

--- Program.cs
+++ Program.cs
@@ -5,8 +5,22 @@ namespace ConsoleApp1
 {
     class Program
     {
+        
 
-        static void Main(string[] args)
+        NumberFormatInfo numberFormatInfo = new NumberFormatInfo
+        {
+            NumberDecimalSeparator = ".",
+        };
+
+        static double firstNumber;
+        static double secondNumber;
+        static double result;
+        static char action;
+        static bool num1 = true;
+        static bool num2 = true;
+        static bool dates = true;
+
+          static void Main(string[] args)
         {
             #region array names
             // Создание массива строк
@@ -29,85 +43,107 @@ namespace ConsoleApp1
             }*/
             #endregion
 
-            #region calculator
 
-            NumberFormatInfo numberFormatInfo = new NumberFormatInfo
-            {
-                NumberDecimalSeparator = ".",
-            };
 
-            double firstNumber;
-            double secondNumber;
-            double result;
-            char action;
-            Console.WriteLine("Enter the first number");
-            double.TryParse(Console.ReadLine(), out firstNumber);
-            Console.WriteLine("Enter the second number");
-            double.TryParse(Console.ReadLine(), out secondNumber);
+
+            EnterDate();
+
             Console.WriteLine("select the action to be performed: '+', '-', '/', '*', '%'");   // '+', '-', '/', '*', '%'
             char.TryParse(Console.ReadLine(), out action);
 
-            //if (action != '+' || action != '-'|| action != '+'||action != '+'||action != '+')
-
-            switch (action)
-            {
-                case '+':
-                    result = firstNumber + secondNumber;
-                    Console.WriteLine("{0}  {1}  {2} = {3}", firstNumber, action, secondNumber, result);
-                    break;
-                case '-':
-                    result = firstNumber - secondNumber;
-                    Console.WriteLine("{0}  {1}  {2} = {3}", firstNumber, action, secondNumber, result);
-                    result = firstNumber - secondNumber;
-                    break;
-                case '*':
-                    result = firstNumber * secondNumber;
-                    Console.WriteLine("{0}  {1}  {2} = {3}", firstNumber, action, secondNumber, result);
-                    result = firstNumber * secondNumber;
-                    break;
-                case '/':
-
-                    if (secondNumber != 0)
-                    {
-                        result = firstNumber / secondNumber;
+            if (action != '+' || action != '-' || action != '+' || action != '+' || action != '+')
+            { 
+                switch (action)
+                {
+                    case '+':
+                        result = firstNumber + secondNumber;
                         Console.WriteLine("{0}  {1}  {2} = {3}", firstNumber, action, secondNumber, result);
+                        dates = num2 = num1 = true;
                         break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Division by zero, action cannot be performed");
-                        break;
-                    }
-                case '%':
-                    if (secondNumber != 0)
-                    {
-                        result = firstNumber % secondNumber;
+                    case '-':
+                        result = firstNumber - secondNumber;
                         Console.WriteLine("{0}  {1}  {2} = {3}", firstNumber, action, secondNumber, result);
+                        result = firstNumber - secondNumber;
+                        dates = num2 = num1 = true;
                         break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Division by zero, action cannot be performed");
+                    case '*':
+                        result = firstNumber * secondNumber;
+                        Console.WriteLine("{0}  {1}  {2} = {3}", firstNumber, action, secondNumber, result);
+                        result = firstNumber * secondNumber;
+                        dates = num2 = num1 = true;
                         break;
-                    }
+                    case '/':
 
-                default:
+                        if (secondNumber != 0)
+                        {
+                            result = firstNumber / secondNumber;
+                            Console.WriteLine("{0}  {1}  {2} = {3}", firstNumber, action, secondNumber, result);
+                            dates = num2 = num1 = true;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Division by zero, action cannot be performed");
+                            break;
+                        }
+                    case '%':
+                        if (secondNumber != 0)
+                        {
+                            result = firstNumber % secondNumber;
+                            Console.WriteLine("{0}  {1}  {2} = {3}", firstNumber, action, secondNumber, result);
+                            dates = num2 = num1 = true;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Division by zero, action cannot be performed");
+                            break;
+                        }
 
-                    Console.WriteLine("Enter valid data and correct actions!");
-                    break;
+                    default:
+
+                        Console.WriteLine("Enter valid data and correct actions!");
+                        break;
+                }
             }
+            else
+            {
+                return;
+            }
+        Console.ReadKey();
 
-
-
-            #endregion
-
-            Console.ReadKey();
-
-
-
-
+            
         }
+       
 
+        private static bool EnterDate()
+        {
+            if (dates)
+            {
+                if (num1)
+                {
+                    Console.WriteLine("Enter the first number");
+                    double.TryParse(Console.ReadLine(), out firstNumber);
 
+                    num1 = false;
+                }
+
+                 if(num2)
+                {
+                    Console.WriteLine("Enter the second number");
+                    double.TryParse(Console.ReadLine(), out secondNumber);
+                    num2 = false;
+                }
+
+                dates = false;
+            }
+            
+            return dates;
+        }
     }
+
 }
+    
+
+
+
